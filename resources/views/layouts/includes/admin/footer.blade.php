bbe0d28f6bbfb5cc57ed41375676c44c253d602a
candra <candraherdiansyah14@gmail.com>
2021-10-11 14:36:27 +0700

    Auth

---
+++ resources/views/layouts/includes/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; {{ date("Y") }} <a href="#">SMK ASSALAAM</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block"><b>Version</b> 3.1.0</div>
+</footer>
